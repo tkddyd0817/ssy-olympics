a71fb5eede989b6cb52e5a5be861f9cdedb500da
tkddyd1259 <tkddyd1259@gmail.com>
2024-11-01 20:01:26 +0900

    이미지 추가완료

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MedalItem from './components/MedalItem'
 import MedalForm from './components/MedalForm'
+import logolmage from '../public/images.png'
 
 
 
@@ -80,6 +81,8 @@ console.log(result);
 
   return (
     <div id='root'>
+      <div><img  src={logolmage}/></div>
+      
       <div className='Main-Container'>
         <h1>파리 올림픽</h1>
         <MedalForm
@@ -100,7 +103,7 @@ console.log(result);
     </div>
   
   )
-}
+};
 
 export default App
 
